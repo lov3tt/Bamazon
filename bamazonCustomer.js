d513d0428bb4cadd0ecbf7d3e7ece51484e2af54
Bobby Quach <BobbyQuach90@gmail.com>
2019-06-05 01:51:03 -0400

    Part1 Finish

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,34 +11,23 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    //  Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-    // connection.end();
+    console.log("connected as guest id" + connection.threadId);
     readProducts();
 });
 
 function readProducts() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(res);
+        console.log("ID# | Products Name : Unit Cost")
+        console.log("-------------------------------------------------------");
+        for (var i = 0; i < res.length; i++)
+        
+        console.log(res[i].item_id + " | " + res[i].product_name + " : $"+ res[i].price);
+        console.log("-------------------------------------------------------");
         initialize()
         // connection.end();
     })
 }
-
-// 6. The app should then prompt users with two messages.
-
-// * The first should ask them the ID of the product they would like to buy.
-// * The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-// * This means updating the SQL database to reflect the remaining quantity.
-// * Once the update goes through, show the customer the total cost of their purchase.
 function initialize() {
     connection.query("SELECT * FROM products", function(err, res){
         if (err) throw err;
@@ -56,37 +45,65 @@ function initialize() {
         .then(function (input) {
             var selectedProduct;
             for (var i = 0; i < res.length; i++) {
-                if (res[i].item_id === input.enterID){
+                if (res[i].item_id == input.enterID){
                     selectedProduct = res[i];
-
                 }
-
-                console.log(res[i])
-                console.log(res[i].item_id)
-                console.log(input.enterID)
-                // console.log(selectedProduct)
             }
-    
-            connection.end()
+            // console.log("input.id: " + input.enterID)
+            // console.log("testing var: " + selectedProduct.product_name)
+            // console.log(res)
+            // connection.end()
+            if (selectedProduct.stock_quantity > parseInt(input.enterAmount)) {
+                console.log("-------------------------------------------------------");
+                console.log("Thanks you for your purchase!")
+                let newStock = selectedProduct.stock_quantity - input.enterAmount;
+                let totalPrice = input.enterAmount * selectedProduct.price
+                // console.log(newStock)
+                // console.log(totalPrice)
+                connection.query( "UPDATE products SET ? WHERE ?", [
+                    {
+                        stock_quantity: newStock
+                    },
+                    {
+                        item_id: input.enterID
+                    }
+                ], function(err, res) {
+                    if (err) throw err;
+                    
+                    console.log("You have ordered: " + input.enterAmount + "x " + selectedProduct.product_name)
+                    console.log("Your total cost is: $" + totalPrice)
+                    console.log("-------------------------------------------------------");
+                    // console.log(res.affectedRows + " products updated")
+                    continueShopping()
+                }
+            
+                );
+            }
+            else {
+                console.log("Insufficient quantity in stock \n Would you like to try a different purchase?")
+                continueShopping()             
+            }
         })
-    })};
-//             if (selectedProduct.stock_quantity > parseInt(input.enterAmount)) {
-//                 console.log("Processing Order")
-//                 completeOrder()
+    })
+}
 
-//             }
-//             else {
-//                 console.log("Insufficient quantity in stock \n Restocking Inventory, try again")
-//                 updateProduct()
-//             }
-//         })
-//     })
-// }
+function continueShopping (){
+    inquirer
+    .prompt([{
+        type: "list",
+        name: "again",
+        message: "Continue Shopping?",
+        choices: ["Yes", "No"],
+    }]).then(function(answer){
+        if (answer.again == "Yes") {
+            readProducts()
+        }
+        else {
+            console.log("Thanks you for shopping at Bamazon")
+            console.log("-------------------------------------------------------");
+            connection.end()
+        }
+    })
 
-// function completeOrder (){
+}
 
-// }
-
-// function updateProduct (){
-
-// }
--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -23,3 +23,30 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Rubber Ducky", "Health&Fitness", 3.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fluffy Rubber Ducky", "Health&Fitness", 6.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Giant Rubber Ducky", "Health&Fitness", 16.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mini Rubber Ducky", "Health&Fitness", 2.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Super Rubber Ducky", "Health&Fitness", 22.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Silly Rubber Ducky", "Health&Fitness", 11.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Hard Rubber Ducky", "Health&Fitness", 8.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Flammable Rubber Ducky", "Health&Fitness", 23.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Squishy Rubber Ducky", "Health&Fitness", 3.50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Golden Rubber Ducky", "Health&Fitness", 999.99, 25);
